40f46b3130158b72a31366b4814ba98dd7a2d905
Eduardo Ferraz <edupferraz@gmail.com>
2022-11-29 06:39:42 -0300

    Post properties

--- Ignite-Feed/src/App.jsx
+++ Ignite-Feed/src/App.jsx
@@ -6,6 +6,27 @@ import styles from './App.module.css'
 
 import './global.css'
 
+// Author { avatar_url; name; role}
+// publishedAt: dat
+// content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/edupferraz.png',
+      name: 'Eduardo Ferraz',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-29 06:21:00')
+  },
+];
+
 export function App() {
 
   return (
@@ -19,15 +40,16 @@ export function App() {
         
         <main>
 
-          <Post 
-            author="Eduardo Ferraz" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
 
-          <Post 
-            author="Diego Fernandes"
-            content="This is a React program"
-          />
         </main>
     </div>
 
