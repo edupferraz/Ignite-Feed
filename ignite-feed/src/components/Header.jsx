f90452ea7173dd984eb75f63d1e7e9936db456fc
Eduardo Ferraz <edupferraz@gmail.com>
2024-07-24 00:01:39 -0300

    Application structure - Header

--- ignite-feed/src/components/Header.jsx
+++ ignite-feed/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/igniteLogo.svg';
+
 export function Header() {
     return (
-        <strong className={styles.header}>Ignite Feed</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt='Ignite Logo'/>
+        </header>
     );
 }
