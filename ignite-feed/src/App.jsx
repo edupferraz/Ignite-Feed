8297c909c8e9f27034d920eb51944487a03ce93d
Eduardo Ferraz <edupferraz@gmail.com>
2024-07-25 23:52:09 -0300

    Application structure - Header and Sidebar

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,7 +1,11 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
+
 
 function App() {
 
@@ -9,10 +13,17 @@ function App() {
     <div>
       <Header/>
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem"
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
