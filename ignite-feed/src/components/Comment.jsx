00e456f61c2e525e875c8d7c08be35407c93c2dc
Eduardo Ferraz <edupferraz@gmail.com>
2024-08-25 15:58:45 -0300

    Application Structure

---
+++ ignite-feed/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/edupferraz.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Eduardo Ferraz</strong>
+              <time title="25 de Agosto às 15:30" dateTime="2024-08-25 15:30:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -23,6 +24,26 @@ export function Post() {
                 <a href="">#novoprojeto</a> {}
                 <a href="">#rocketseat</a> 
             </p>
+            </div>
+
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment/>
+                <Comment />
+                <Comment />
             </div>
         </article>
 
