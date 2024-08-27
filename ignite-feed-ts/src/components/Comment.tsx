b7a7fe1a70514cd49e78f418ad0a37787d34d688
Eduardo Ferraz <edupferraz@gmail.com>
2024-08-26 23:01:47 -0300

    Improving the application

---
+++ ignite-feed-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/edupferraz.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Eduardo Ferraz</strong>
+              <time title="25 de Agosto às 15:30" dateTime="2024-08-25 15:30:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
---
+++ ignite-feed-ts/src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/57379069?v=4"/>
+
+                <strong>Eduardo Ferraz</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
